d56437803d76acddd89ce619290d9d1520df34e0
faizanv <faizan.virani.44@gmail.com>
2016-01-07 18:46:30 -0500

    Finished most expense endpoints that hit Parse

--- AbyServer/server/routes/users.js
+++ AbyServer/server/routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var async = require('async');
 var request = require('superagent');
+var Parse = require('parse/node');
+Parse.initialize("KCRcO4MK7dW8maRqktTwyXGswsP8NGxNC5QsnAaH", "nQBdEWP0OpyNSZNpbzJ9N8PdJtXTF1mS0L4Q6X9S");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -23,7 +25,102 @@ router.get('/balance', function(req, res, next) {
           checkingBal = response[i].balance;
         }
       }
-      res.send(JSON.stringify({current: (checkingBal - creditBal)}));
+      res.json({current: (checkingBal - creditBal)});
+    }
+  });
+});
+
+router.get('/dayExpenses', function(req, res, next) {
+  //get total expenses for a given day
+  res.send("I don't know what I'm doing");
+});
+
+//Get all expenses from Parse
+router.get('/expense', function(req, res, next) {
+  var Expense = Parse.Object.extend("Expense");
+  var query = new Parse.Query(Expense);
+  query.find({
+    success: function(result) {
+      var out = [];
+      for (i = 0; i < result.length; i++) {
+        var cur = result[i];
+        console.log(cur.id);
+        var temp = {
+          id: cur.id,
+          amount: cur.get("amount"),
+          date: cur.get("date"),
+          title: cur.get("title")
+        }
+        out.push(temp);
+      }
+      res.json(out);
+    },
+    error: function(err) {
+      res.send(err);
+    }
+  });
+});
+
+//Get expense by id
+router.get('/expense/:id', function(req, res, next) {
+  var Expense = Parse.Object.extend("Expense");
+  var query = new Parse.Query(Expense);
+  query.get(req.params.id, {
+    success: function(cur) {
+      var temp = {
+        id: cur.id,
+        amount: cur.get("amount"),
+        date: cur.get("date"),
+        title: cur.get("title")
+      }
+      res.json(temp);
+    },
+    error: function(something, error) {
+      res.send(error);
+    }
+  });
+});
+
+//POST new expense to Parse
+router.post('/expense', function(req, res, next) {
+  var body = req.body;
+  if (!body) {
+    res.send("Request body empty");
+  }
+  var Expense = Parse.Object.extend("Expense");
+  var newExpense = new Expense();
+  var data = {
+    date: body.date,
+    title: body.title,
+    amount: body.amount
+  }
+  newExpense.save(data, {
+    success: function(ret) {
+      res.send("Expense has been pushed to Parse");
+    },
+    error: function(something, err) {
+      res.send(err);
+    }
+  });
+});
+
+//DELETE expense by id
+router.delete('/expense/:id', function(req, res, next) {
+  var Expense = Parse.Object.extend("Expense");
+  var query = new Parse.Query(Expense);
+  query.get(req.params.id, {
+    success: function(exp) {
+      exp.destroy({
+        success: function(obj) {
+          res.send("Deletion success");
+        },
+        error: function(obj, error) {
+          res.send(error);
+        }
+      });
+    },
+    error: function(something, error) {
+      res.send(error);
     }
   });
 });
