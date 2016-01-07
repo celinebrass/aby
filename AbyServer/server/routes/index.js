29f70aa1bbac8e76eb96ce1532b03f02f0f854e2
Celine Brass <cjb327@cornell.edu>
2016-01-07 12:10:42 -0500

    api calls now possible.  using Celine's api key

--- AbyServer/server/routes/index.js
+++ AbyServer/server/routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var async = require('async');
 var request = require('superagent');
 
-var key = f8608da12d10def4ff1509075e4e5cd;
+var key = "f8608da12d10def4ff1509075e4e5cd";
+
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    request.get('http://api.reimaginebanking.com/atms?lat=38.9283&lng=-77.1753&rad=1&key=f8608da12d10def4ff1509075e4e5cd5').end(function(err, response){
+    	console.log(response.status);
+    	console.log(response.body);
+    	res.render('index', { title: 'Express' });
+	});
 });
 
 module.exports = router;
